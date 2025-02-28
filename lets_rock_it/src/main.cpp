f52e9e687b067bf327daee6d6f134502069ee0f8
Vondras <vojtaviolka@seznam.cz>
2025-02-28 14:55:38 +0100

    First measurements

--- lets_rock_it/src/main.cpp
+++ lets_rock_it/src/main.cpp
@@ -6,10 +6,22 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(A0, INPUT_ANALOG);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+
+  int measuredValue = analogRead(A0);
+  Serial.println(measuredValue);
+
 }
 
 // put function definitions here:
