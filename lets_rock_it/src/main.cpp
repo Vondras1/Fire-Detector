4875417f92e4476fd8c13b665e69b1828107fb8c
Petr Pojar <pojar.petr@seznam.cz>
2025-04-13 16:20:06 +0200

    added SD card data saving to lora_transmitter

--- lets_rock_it/src/main.cpp
+++ lets_rock_it/src/main.cpp
@@ -1,54 +1,148 @@
-#include <Arduino.h>
+/*
+  SD card read/write
 
-// put function declarations here:
-int countAverage(int, float a, bool first);
-void measureBattery(int *vbat);
-bool first = true;
+  This example shows how to read and write data to and from an SD card file
+  The circuit:
+   SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
 
+  created   Nov 2010
+  by David A. Mellis
+  modified 9 Apr 2012
+  by Tom Igoe
 
-#define FlameSensorPin A3
-#define BatteryPin A4
-#define DIVIDER_RATIO 0.0054149 // 1M/1.47M resistor divider, 3.3V FS, 10 bit ADC
+  This example code is in the public domain.
+
+*/
+
+#include "Arduino.h"
+#include <SPI.h>
+#include "SD.h"
+
+#define CSPIN 9
+
+//functions 
+int getNumFiles(File dir);
+bool writeData(File mf, int smoke, int flame, int gas);
+bool writeHeader(File mf, String header);
+
+File myFile;
+String full_filename;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  String filename = "tabor"; // default file name, can be changed
 
-  pinMode(A0, INPUT_ANALOG);
-  pinMode(BatteryPin, INPUT_ANALOG);
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(CSPIN)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  
+  File root = SD.open("/");
+
+  int num_files = getNumFiles(root);
+
+  Serial.println("Counter: " + String(num_files));
+
+  full_filename = filename + String(num_files) + String(".txt") ;
+  
+  Serial.println(full_filename);
+
+  myFile = SD.open(full_filename, FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing header to " + full_filename);
+    // write header to file
+    writeHeader(myFile, "smoke,flame,gas");
+    // close the file:
+    myFile.close();
+    Serial.println(" done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening file");
+  }
+  myFile.close();
+
+  /*
+  // re-open the file for reading:
+  myFile = SD.open(full_filename);
+  if (myFile) {
+    Serial.println(full_filename + ": ");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening " + full_filename);
+  }
+  */
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // digitalWrite(LED_BUILTIN, HIGH);
-  // delay(500);
-  // digitalWrite(LED_BUILTIN, LOW);
-  // delay(500);
 
-  int v_bat;
-  measureBattery(&v_bat);
-  Serial.print("Battery voltage: ");
-  Serial.println((float)v_bat*DIVIDER_RATIO);
+  myFile = SD.open(full_filename, FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to " + full_filename);
+    int smoke = random(1,1024), flame = random(1,1024), gas = random(1,1024);
+    // write to file
+    writeData(myFile, smoke, flame, gas);
+    // close the file:
+    myFile.close();
+    Serial.println(" done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening file");
+  }
+  myFile.close();
 
   delay(1000);
 }
 
-// put function definitions here:
-int countAverage(int x, float a=0.8, bool first=false) {
-  static float avg;
-  if (first){avg = x;}
-  avg = a*x + (1-a)*avg;
-  return (int)avg;
+int getNumFiles(File dir) {
+  int cntr = 0;
+  while (true) {
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      break;
+    }
+    cntr++;
+    entry.close();
+  }
+  return cntr;
+
 }
 
-void measureBattery(int *vbat){
-  const int num_iter = 4;
-  int sum_vbat = 0;
-  for (int i = 0; i < num_iter; i++){
-    sum_vbat += analogRead(BatteryPin);
-    delay(2);
-  }
-  *vbat = sum_vbat/num_iter;
-  //Serial.println(analogRead(BatteryPin));
+bool writeData(File mf, int smoke, int flame, int gas){
+  mf.print(smoke);
+  mf.print(',');
+  mf.print(flame);
+  mf.print(',');
+  mf.print(gas);
+  mf.print('\n');
+  return true;
+}
+
+bool writeHeader(File mf, String header){
+  mf.println(header);
+  return true;
 }
